39eca6bc9467f5f6f7e9dec8fdc075349df1d394
DomenicoLucarelli <domyluca@gmail.com>
2023-03-28 19:23:28 +0200

    bonus

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,18 +26,29 @@ export default {
 </script>
 
 <template>
-    <div class="deck">
+    <div class="loading" v-if="store.cards.length < 50">
+        <strong>LOADING</strong>
+    </div>
+
+    <div class="deck" v-else="">
 
         <MainCard v-for="(card, index) in store.cards" :image="card.card_images[0].image_url" :card-name="card.name" >
 
         </MainCard>
-
-       
-
     </div>
 </template>
 
 <style lang="scss" scoped>
+.loading{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4em;
+    color: red;
+
+}
 .deck {
     display: flex;
     flex-wrap: wrap;
